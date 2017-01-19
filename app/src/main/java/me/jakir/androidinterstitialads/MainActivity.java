534441d4a959d0b68d03b739f6633f85cdf947ec
jakir hossain <jakir hossain>
2017-01-20 00:45:46 +0600

    Show Ad

---
+++ app/src/main/java/me/jakir/androidinterstitialads/MainActivity.java
@@ -0,0 +1,57 @@
+package me.jakir.androidinterstitialads;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mButton;
+    InterstitialAd mInterstitialAd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        requestNewInterstitial();
+
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+
+            }
+        });
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Ad did not load", Toast.LENGTH_SHORT).show();
+                    requestNewInterstitial();
+                }
+            }
+        });
+    }
+
+    private void requestNewInterstitial() {
+        if (!mInterstitialAd.isLoading() && !mInterstitialAd.isLoaded()) {
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mInterstitialAd.loadAd(adRequest);
+        }
+    }
+}
