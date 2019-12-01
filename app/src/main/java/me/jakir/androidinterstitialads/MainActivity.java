b9461a98f24303871eb0f8fe26833b4c6a6ff426
Jakir Hossain <jakirseu@gmail.com>
2019-12-01 13:28:34 +0400

    update project

--- app/src/main/java/me/jakir/androidinterstitialads/MainActivity.java
+++ app/src/main/java/me/jakir/androidinterstitialads/MainActivity.java
@@ -1,10 +1,12 @@
 package me.jakir.androidinterstitialads;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
